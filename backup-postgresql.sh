4229e145b764b2af2dc1e33639363c2385174bab
Jan Fader <jan.fader@web.de>
2015-04-01 22:46:47 +0200

    removed debug code from backup-postgresql.sh

--- backup-postgresql.sh
+++ backup-postgresql.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 mkdir -p /backup/coreos-2-p/postgresql/
-echo "TEST1"
-ssh core@coreos-2-p.piratenpartei.ch docker run -i --link members-pgsql:db -e PGPASSWORD=$PGPASSWORD postgres pg_dump -h db -U postgres -d memberservice | gzip > /backup/coreos-2-p/postgresql/memberservice-$(date +%Y-%m-%d).gz
-echo "TEST2"
+tssh core@coreos-2-p.piratenpartei.ch docker run -i --link members-pgsql:db -e PGPASSWORD=$PGPASSWORD postgres pg_dump -h db -U postgres -d memberservice | gzip > /backup/coreos-2-p/postgresql/memberservice-$(date +%Y-%m-%d).gz
