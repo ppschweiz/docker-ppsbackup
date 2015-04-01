cb4673aeb1710d0a624b59611bf18c079c4843fa
Jan Fader <jan.fader@web.de>
2015-04-01 23:29:23 +0200

    correted typo in backup-postgresql.sh

--- backup-postgresql.sh
+++ backup-postgresql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /backup/coreos-2-p/postgresql/
-tssh core@coreos-2-p.piratenpartei.ch docker run -i --link members-pgsql:db -e PGPASSWORD=$PGPASSWORD postgres pg_dump -h db -U postgres -d memberservice | gzip > /backup/coreos-2-p/postgresql/memberservice-$(date +%Y-%m-%d).gz
+ssh core@coreos-2-p.piratenpartei.ch docker run -i --link members-pgsql:db -e PGPASSWORD=$PGPASSWORD postgres pg_dump -h db -U postgres -d memberservice | gzip > /backup/coreos-2-p/postgresql/memberservice-$(date +%Y-%m-%d).gz
