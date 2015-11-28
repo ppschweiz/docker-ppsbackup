68ed42dc14ca73fdd51367497f379340c910c47e
Jan Fader <jan.fader@web.de>
2015-11-28 16:22:43 +0100

    added delete-function and monthly backup to different folder for mysql-backups

--- rsync-mysql.sh
+++ rsync-mysql.sh
@@ -2,3 +2,15 @@
 
 mkdir -p /backup/mysql-1-p/
 rsync -av mysql-1-p.piratenpartei.ch:/var/backup/* /backup/mysql-1-p/
+
+# create monthbackup
+if [ $(date %d) -eq 01 ]; then
+  mkdir -p /backup/mysql-1-p/monatssicherung/
+  find /backup/mysql-1-p/mysql -type f -ctime -1 -exec cp {} /backup/mysql-1-p/monatssicherung/
+
+  # cleanup monthly backups after 365 days
+  find /backup/mysql-1-p/monatssicherung -type -f -ctime +365 -delete
+fi
+
+# cleanup older than 45 days
+find /backup/mysql-1-p/mysql -type f -ctime +45 # -delete
