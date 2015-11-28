f1726015a4411861f3dda33b0c350660a5a819f6
Jan Fader <jan.fader@web.de>
2015-11-29 00:17:05 +0100

    added echos for logs

--- rsync-mysql.sh
+++ rsync-mysql.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# cleanup older than 45 days
+echo "cleanup normal backups older than 45 days"
 find /backup/mysql-1-p/mysql -type f -ctime +45 -delete
 
 mkdir -p /backup/mysql-1-p/
 rsync -av mysql-1-p.piratenpartei.ch:/var/backup/* /backup/mysql-1-p/
 
-# create monthbackup
 if [ $(date +%d) -eq 01 ]; then
+  echo "creating backup for month $(date "+%B %Y")"
   mkdir -p /backup/mysql-1-p/monatssicherung/
   find /backup/mysql-1-p/mysql -type f -ctime -1 -exec cp {} /backup/mysql-1-p/monatssicherung/
 
-  # cleanup monthly backups after 365 days
+  echo "cleanup monthly backups after 365 days"
   find /backup/mysql-1-p/monatssicherung -type -f -ctime +365 -delete
 fi
