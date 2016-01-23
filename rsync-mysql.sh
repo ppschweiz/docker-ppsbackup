e4f4d7a6f10c7ce3d027ca3f2f423aeab37b6841
Jan Fader <jan.fader@web.de>
2016-01-23 10:12:22 +0100

    fixed monthly backup

--- rsync-mysql.sh
+++ rsync-mysql.sh
@@ -9,7 +9,7 @@ rsync -av mysql-1-p.piratenpartei.ch:/var/backup/* /backup/mysql-1-p/
 if [ "x$(date +%d)x" = "x01x" ]; then
   echo "creating backup for month $(date "+%B %Y")"
   mkdir -p /backup/mysql-1-p/monatssicherung/
-  find /backup/mysql-1-p/mysql -type f -ctime -1 -exec cp {} /backup/mysql-1-p/monatssicherung/
+  find /backup/mysql-1-p/mysql -type f -ctime -1 -exec cp {} /backup/mysql-1-p/monatssicherung/ \;
 
   echo "cleanup monthly backups after 365 days"
   find /backup/mysql-1-p/monatssicherung -type -f -ctime +365 -delete
